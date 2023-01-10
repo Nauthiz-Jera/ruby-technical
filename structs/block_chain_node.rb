8f19cf6ce51a555c84f0ba60b0e088a6be29d635
Nauthiz.Jera <Nauthiz.Jera@gmail.com>
2023-01-10 15:32:24 -0500

    convert struct from hash

--- structs/block_chain_node.rb
+++ structs/block_chain_node.rb
@@ -1,19 +1,19 @@
 require 'sorbet-runtime'
 
 class NodeResult < T::Struct
-    const :blockHash, T.nilable(String)
-    const :blockNumber, T.nilable(String)
-    const :transaction_hash, T.nilable(String)
-    const :from, T.nilable(String)
-    const :gas, T.nilable(String)
-    const :gasPrice, T.nilable(String)
-    const :input, T.nilable(String)
-    const :nonce, T.nilable(String)
-    const :r, T.nilable(String)
-    const :s, T.nilable(String)
-    const :to, T.nilable(String)
-    const :transactionIndex, T.nilable(String)
-    const :type, T.nilable(String)
-    const :v, T.nilable(String)
-    const :value, T.nilable(String)
+    const :blockHash, String
+    const :blockNumber, String
+    const :transaction_hash, String
+    const :from, String
+    const :gas, String
+    const :gasPrice, String
+    const :input, String
+    const :nonce, String
+    const :r, String
+    const :s, String
+    const :to, String
+    const :transactionIndex, String
+    const :type, String
+    const :v, String
+    const :value, String
 end
